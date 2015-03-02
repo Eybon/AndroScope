9949d799f9ae046de963d34609abdef50e47fdc5
eybon <antoine.forgerou@gmail.com>
2015-03-02 19:02:03 +0100

    Initial commit

---
+++ gen/R.java.d
@@ -0,0 +1,8 @@
+/home/antoine/Android/AndroScope/gen/io/github/eybon/AndroScope/R.java \
+ : /home/antoine/Android/AndroScope/res/drawable-hdpi/ic_launcher.png \
+/home/antoine/Android/AndroScope/res/layout/main.xml \
+/home/antoine/Android/AndroScope/res/drawable-mdpi/ic_launcher.png \
+/home/antoine/Android/AndroScope/res/drawable-xhdpi/ic_launcher.png \
+/home/antoine/Android/AndroScope/res/drawable-ldpi/ic_launcher.png \
+/home/antoine/Android/AndroScope/res/values/strings.xml \
+/home/antoine/Android/AndroScope/bin/AndroidManifest.xml \
